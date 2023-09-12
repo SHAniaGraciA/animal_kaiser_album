a467f2d7653981ab0fdea2fbc16765073f90c174
Natanael Bonaparte <natanael.bonaparte@ui.ac.id>
2023-09-13 00:33:25 +0700

    Add README, Add Test

--- main/tests.py
+++ main/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase, Client
+from .models import Item
 
 class mainTest(TestCase):
     def test_main_url_is_exist(self):
@@ -8,3 +9,16 @@ class mainTest(TestCase):
     def test_main_using_main_template(self):
         response = Client().get('/main/')
         self.assertTemplateUsed(response, 'main.html')
+
+class ItemTest(TestCase):
+    def test_create_item(self):
+        item = Item.objects.create(
+            name='Test Item',
+            description='This is a test item',
+            category='Test Category',
+            amount=10,
+        )
+        retrieved_item = Item.objects.get(name='Test Item')
+
+        self.assertEqual(item, retrieved_item)
+
