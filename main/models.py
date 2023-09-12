3601efbd7386484ddb7f4e4562e1c2c49585a960
Natanael Bonaparte <natanael.bonaparte@ui.ac.id>
2023-09-12 19:25:36 +0700

    Make new models, make background color, make table

--- main/models.py
+++ main/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 
 class Item(models.Model):
+    char_name = models.CharField(max_length=255, default='Bonaparte')
+    role = models.CharField(max_length=255, default='Stealth Assasin')
     name = models.CharField(max_length=255)
-    amount = models.IntegerField()
     description = models.TextField()
     category = models.CharField(max_length=255)
+    amount = models.IntegerField()
--- main/views.py
+++ main/views.py
@@ -3,8 +3,38 @@ from django.shortcuts import render
 # Create your views here.
 def show_main(request):
     context = {
-        'name': 'Bonaparte',
-        'class': 'Stealth Assasin'
+        'char_name' : 'Bonaparte',
+        'role': 'Stealth Assasin',
+        'items': [
+            {
+                'name': 'Smoke Bomb',
+                'description': 'Use it to easily dissapear from enemy sight ',
+                'category': 'Throwable',
+                'amount': 5,
+                
+            },
+            {
+                'name': 'Bandage',
+                'description': 'Use it to heal your wound and recover 35 HP',
+                'category': 'Consumable',
+                'amount': 3,
+        
+            },
+            {
+                'name': 'Money',
+                'description': 'Use it to fulfill your needs without rob & kill',
+                'category': 'Tradeable',
+                'amount': 900,
+            },
+            {
+                'name': 'Golden Chest',
+                'description': 'Open it to get some rewards after completing mission',
+                'category': 'Other',
+                'amount': 10,
+            },
+            
+        ]
     }
+    
 
     return render(request, "main.html", context)
